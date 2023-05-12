0db132173123432298755ed1ac05f86d30244219
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-05-12 19:08:26 +0200

    update

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
-"""Description. Compute PageRank values and visualize the graph.
-
-Example: `python main.py -n 1 -save True`
-
-Initial PR values: [1. 1. 1. 1. 1. 1.]
-Computing PageRank with damping_factor=0.15: 100%|███████████████████████████████████████| 100/100 [00:00<00:00, 37858.15it/s]
-Updated PR values with d=0.15 after 100 iterations: [0.061 0.112 0.156 0.193 0.225 0.252]"""
-
-from src.utils import file_to_edges, compute_time
+from src.utils import compute_time, generate_random_edges
 from src.graph import Graph
 from src.pagerank import compute_pagerank
 
 import rich
 import argparse
 
-def main(graph_number: int, d: float, n_iter: int, save: bool): 
-    edges = file_to_edges(f"datasets/graph_{graph_number}.txt")
+def main(n_edges: int, d: float, n_iter: int, save: bool): 
+    edges = generate_random_edges(n_edges)
     graph = Graph.from_edges(edges)
 
-    rich.print(f"Initial PR values: {graph.get_pagerank_list()}")
-
-    time = compute_time(compute_pagerank, graph=graph, d=d, n_iter=100) 
-    rich.print(f"Updated PR values with {d=} after {n_iter} iterations: {graph.get_pagerank_list()}") 
-    rich.print(f"Time taken to compute PageRank: {time:.3f} seconds")
+    rich.print(f"{n_edges} random edges in the graph.")
+    result = compute_time(compute_pagerank, graph=graph, d=d, n_iter=100) 
+    rich.print(f"Execution time summary: {result}")
 
     if save:
-        graph.visualize(figure_file=f"figs/graph_{graph_number}.png")
+        figure_file=f"figs/graph_{n_edges}.png"
+        pageranks = graph.get_pageranks(save_path=f"backup/pageranks_{n_edges}.json")
     else: 
-        graph.visualize()
+        figure_file = None
+        pageranks= graph.get_pageranks()
+
+    rich.print(f"Updated PR values with {d=} after {n_iter} iterations: {pageranks}") 
+    graph.visualize(figure_file=figure_file)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--graph_number", default=1)
+parser.add_argument("-n", "--n_edges", default=10)
 parser.add_argument("-d", "--d", default=0.85)
 parser.add_argument("-n_iter", "--n_iter", default=100)
 parser.add_argument("-save", "--save", default=False)
 args = parser.parse_args()
 
-main(int(args.graph_number), float(args.d), int(args.n_iter), args.save)
+main(int(args.n_edges), float(args.d), int(args.n_iter), args.save)
