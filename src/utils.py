2b8a2178991510bd7aeaca66411d92866e873f93
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-05-16 20:24:32 +0200

    add make_computation_time_table, make_mse_table

--- src/utils.py
+++ src/utils.py
@@ -1,43 +1,10 @@
-from typing import Optional, Dict, Callable, List
+from typing import Optional, Dict, Callable, List, Union
 
 import numpy as np
 import pandas as pd
 from tqdm import tqdm 
 import time 
-import os 
 
-import json
-import yaml
-from yaml import Loader
-
-
-def to_json(results: Dict, dir_path: str, file_name: str) -> None: 
-
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
-    file_path = os.path.join(dir_path, file_name)
-
-    with open(file_path, "w") as f:
-        json.dump(results, f)
-
-    print(f"Saved results to {file_path}.")
-
-def from_json(dir_path: str, file_name: str) -> Dict: 
-    file_path = os.path.join(dir_path, file_name)
-
-    with open(file_path, "r") as f:
-        results = json.load(f)
-
-    return results
-
-def load_config(cfg_path: str) -> Dict:
-    """Load yaml configuration file."""
-    
-    with open(cfg_path, "r") as ymlfile:
-        cfg = yaml.load(ymlfile, Loader)
-
-    return cfg
 
 def measure_time(func: Callable, n_repeat: int=50, return_results: bool=False, desc: Optional[str]=None, *args, **kwargs):
     """Returns the time taken to run a function n_repeat times.
@@ -78,47 +45,51 @@ def measure_time(func: Callable, n_repeat: int=50, return_results: bool=False, d
     
     return time_result
 
-def compute_mse(x: np.ndarray, y: np.ndarray) -> float:
+def compute_mse(x: Union[np.ndarray, List], y: Union[np.ndarray, List]) -> float:
     """Returns the mean squared error between two arrays."""
 
     if len(x) != len(y):
         raise ValueError("Arrays must be of same length")
+    
+    if isinstance(x, list):
+        x = np.array(x)
+    
+    if isinstance(y, list):
+        y = np.array(y)
 
     mse = np.sum((x - y) ** 2) / len(x)
     return mse
 
-def flatten_results(results: Dict) -> List:
-    """Flattens the results of an experiment into a list of dictionaries.
+def flatten_results(results: Dict, method: str) -> Dict:
+    """Flattens the nested results of an experiment into a flat dictionnary.
     
     Args:
         results (Dict): The results of an experiment.
+        method (str): The method used to generate the results.
         
     Returns:
-        List: A list of dictionaries containing the results of the experiment per method."""
+        Dict: A flat dictionary containing the results of the experiment per method."""
 
-    results_flat = []
+    row = {
+        "method": method,
+        "n_nodes": results["n_nodes"],
+        "min_conn_per_node": results["min_conn_per_node"],
+        "max_iter": results["max_iter"],
+        "min_time": results[f"time_{method}"]["min"],
+        "max_time": results[f"time_{method}"]["max"],
+        "mean_time": results[f"time_{method}"]["mean"],
+        "median_time": results[f"time_{method}"]["median"],
+        "std_time": results[f"time_{method}"]["std"]
+    }
+       
+    return row
 
-    for method in ["numpy", "cython"]:
-        row = {
-            "method": method,
-            "n_nodes": results["n_nodes"],
-            "min_conn_per_node": results["min_conn_per_node"],
-            "max_iter": results["max_iter"],
-            "min_time": results[f"time_{method}"]["min"],
-            "max_time": results[f"time_{method}"]["max"],
-            "mean_time": results[f"time_{method}"]["mean"],
-            "median_time": results[f"time_{method}"]["median"],
-            "std_time": results[f"time_{method}"]["std"]
-        }
-        results_flat.append(row)  
-
-    return results_flat
-
-def dict_to_dataframe(results: Dict) -> pd.DataFrame:
-    """Converts the results of an experiment into a pandas DataFrame.
+def make_computation_time_table(results: List, method: str) -> pd.DataFrame:
+    """Returns a pandas DataFrame containing the computation times of an experiment for a given method.
     
     Args:
-        results (Dict): The results of an experiment.
+        results (List): The results of an experiment.
+        method (str): The method used to generate the results.
         
     Returns:
         pd.DataFrame: A pandas DataFrame containing the results of the experiment per method."""
@@ -128,7 +99,43 @@ def dict_to_dataframe(results: Dict) -> pd.DataFrame:
     )
 
     for item in results:    
-        item_flat = flatten_results(item)
+        item_flat = flatten_results(item, method)
         df = df.append(item_flat, ignore_index=True)
         
+    return df
+
+def make_mse_table(results_per_method: Dict) -> pd.DataFrame:
+    """Returns a pandas DataFrame containing the MSE of an experiment for a given method.
+    
+    Args:
+        results (List): The results of an experiment.
+        
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the results of the experiment per method."""
+    
+    df = pd.DataFrame(
+        columns=["method", "n_nodes", "min_conn_per_node", "max_iter", "mse"]
+    )
+
+    numpy_pageranks = [
+        item["pagerank_numpy"] for item in results_per_method["numpy"]
+    ]
+
+    for method, values in results_per_method.items():  
+
+        if method == "numpy":
+            pass 
+
+        else: 
+            for ix, item in enumerate(values) :
+                mse = compute_mse(item[f"pagerank_{method}"], numpy_pageranks[ix])
+                row = {
+                    "method": method,
+                    "n_nodes": item["n_nodes"],
+                    "min_conn_per_node": item["min_conn_per_node"],
+                    "max_iter": item["max_iter"],
+                    "mse": mse
+                }
+                df = df.append(row, ignore_index=True)     
+
     return df
