5644db0f62c7ab798cac2722698595a023076a70
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-05-17 23:38:24 +0200

    fix bugs

--- src/pagerank/pagerank_gpu.py
+++ src/pagerank/pagerank_gpu.py
@@ -6,36 +6,37 @@ import scipy.sparse
 import numpy as np
 
 
-def make_pagerank_kernel(N: int) -> cuda.Function: 
+def make_pagerank_kernel(num_nodes: int) -> cuda.Function: 
     """Creates the CUDA kernel for the PageRank algorithm.
     
     Parameters:
-        N (int): The number of nodes in the graph.
+        num_nodes (int): The number of nodes in the graph.
         
     Returns:
         pycuda.driver.Function: The CUDA kernel for the PageRank algorithm."""
 
     pagerank_kernel_code = """
-        __global__ void pagerank_kernel(float *pagerank, const int *edges, const int *indptr, const int *out_degree, const float damping_factor)
+        __global__ void pagerank_kernel(float *pageranks, const int *edges, const int *indptr, const int *out_degree, const float damping_factor)
         {
             const int i = blockIdx.x * blockDim.x + threadIdx.x;
-            const int N = %(N)s;
+            const int num_nodes = %(num_nodes)s;
             const float one_minus_damping = 1.0f - damping_factor;
 
-            if (i < N) {
+            if (i < num_nodes) {
                 float sum = 0.0f;
                 for (int j = indptr[i]; j < indptr[i+1]; j++) {
                     const int neighbor = edges[j];
                     const int neighbor_degree = out_degree[neighbor];
-                    sum += pagerank[neighbor] / neighbor_degree;
+                    sum += pageranks[neighbor] / neighbor_degree;
                 }
-                pagerank[i] = one_minus_damping / N + damping_factor * sum;
+                pagerank[i] = one_minus_damping / num_nodes + damping_factor * sum;
             }
         }
-    """ % {"N": N}
+    """ % {"num_nodes": num_nodes}
     pagerank_kernel = SourceModule(pagerank_kernel_code).get_function("pagerank_kernel")
+    return pagerank_kernel
 
-def compute_pagerank_gpu(graph: scipy.sparse.coo_matrix, damping_factor: float=.85, max_iter: int=100, tol: float=1e-6):
+def compute_pagerank_gpu(graph: scipy.sparse.coo_matrix, damping_factor: float=.85, max_iter: int=100):
     """Computes the PageRank score of each node in the graph with GPU parallelization using PyCUDA.
 
     Parameters:
@@ -47,28 +48,23 @@ def compute_pagerank_gpu(graph: scipy.sparse.coo_matrix, damping_factor: float=.
     Returns:
         numpy.ndarray: The PageRank scores of each node in the graph."""
 
-    # Convert the graph to the CSR format
     graph = graph.tocsr()
-    N = graph.shape[0]
+    num_nodes = graph.shape[0]
 
     edges = graph.indices.astype(np.int32)
     indptr = graph.indptr.astype(np.int32)
     out_degree = np.diff(indptr).astype(np.int32)
 
-    pr = np.ones(N, dtype=np.float32) / N
-    last_pr = np.zeros(N, dtype=np.float32)
-    pagerank_kernel = make_pagerank_kernel(N)
+    pageranks = np.ones(num_nodes, dtype=np.float32) / num_nodes
+    last_pageranks = np.zeros(num_nodes, dtype=np.float32)
+    pagerank_kernel = make_pagerank_kernel(num_nodes)
 
     for _ in range(max_iter):
-        pagerank_kernel(cuda.InOut(pr), cuda.In(edges), cuda.In(indptr), cuda.In(out_degree), np.float32(damping_factor)) 
+        pagerank_kernel(cuda.InOut(pageranks), cuda.In(edges), cuda.In(indptr), cuda.In(out_degree), np.float32(damping_factor))    
+        last_pageranks[:] = pageranks
         
-        if np.abs(pr - last_pr, np.inf).max() < tol:
-            break
-    
-        last_pr[:] = pr
-        
-    pr_sum = pr.sum()
-    if pr_sum > 0:
-        pr /= pr_sum
+    pageranks_sum = pageranks.sum()
+    if pageranks_sum > 0:
+        pageranks /= pageranks_sum
 
-    return pr
+    return pageranks
