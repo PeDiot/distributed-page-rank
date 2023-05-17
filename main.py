fe35fe5e6eb54f806adf2b163d2bf721a8ab5cdf
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-05-17 23:38:43 +0200

    update

--- main.py
+++ main.py
@@ -23,26 +23,39 @@ def main(type: str):
 
         exp.init_graph_memory()
 
-        results_base = exp.run(pagerank_method=compute_pagerank_base, n_repeat=cfg["n_repeat"])
+        results_base = exp.run(
+            pagerank_method=compute_pagerank_base, 
+            damping_factor=cfg["damping_factor"], 
+            n_repeat=cfg["n_repeat"])
+        
         file_name = f"base_{n_nodes}_{min_conn_per_node}_{max_iter}"
         to_json(results_base, "backup", f"{file_name}.json")
 
-        results_numpy = exp.run(pagerank_method=compute_pagerank_numpy, n_repeat=cfg["n_repeat"])
+        results_numpy = exp.run(
+            pagerank_method=compute_pagerank_numpy, 
+            damping_factor=cfg["damping_factor"], 
+            n_repeat=cfg["n_repeat"])
+        
         file_name = f"numpy_{n_nodes}_{min_conn_per_node}_{max_iter}"
         to_json(results_numpy, "backup", f"{file_name}.json")
 
-        results_cython = exp.run(pagerank_method=compute_pagerank_cython, n_repeat=cfg["n_repeat"], num_threads=cfg["num_threads"])
-        file_name = f"cython_{n_nodes}_{min_conn_per_node}_{max_iter}"
-        to_json(results_cython, "backup", f"{file_name}.json")
+        # results_cython = exp.run(
+        #   pagerank_method=compute_pagerank_cython, 
+        #   damping_factor=cfg["damping_factor"], 
+        #   n_repeat=cfg["n_repeat"], 
+        #   num_threads=cfg["num_threads"])
+
+        # file_name = f"cython_{n_nodes}_{min_conn_per_node}_{max_iter}"
+        # to_json(results_cython, "backup", f"{file_name}.json")
 
     elif type == "eval":
         base_results = from_json("backup", f"base_{n_nodes}_{min_conn_per_node}_{max_iter}.json")
         numpy_results = from_json("backup", f"numpy_{n_nodes}_{min_conn_per_node}_{max_iter}.json")
-        cython_results = from_json("backup", f"cython_{n_nodes}_{min_conn_per_node}_{max_iter}.json")
+        # cython_results = from_json("backup", f"cython_{n_nodes}_{min_conn_per_node}_{max_iter}.json")
 
         base_time_table = make_computation_time_table(base_results, "base")
         numpy_time_table = make_computation_time_table(numpy_results, "numpy")
-        cython_time_table = make_computation_time_table(cython_results, "cython")
+        # cython_time_table = make_computation_time_table(cython_results, "cython")
 
         plot_title = f"{n_nodes=} | {min_conn_per_node=} | {max_iter=}"
         figure_file = f"{n_nodes}_{min_conn_per_node}_{max_iter}.png"
@@ -50,7 +63,7 @@ def main(type: str):
             {
                 "base": base_results,
                 "numpy": numpy_results, 
-                "cython": cython_results
+                # "cython": cython_results
             }
         )
         rich.print(mse_table)
@@ -58,7 +71,7 @@ def main(type: str):
         plot_mse(mse_table, "n_nodes", plot_title, f"figs/mse/{figure_file}")
 
         plot_computation_times(
-            results=[base_time_table, numpy_time_table, cython_time_table], 
+            results=[base_time_table, numpy_time_table], 
             x_var="n_nodes", 
             title=plot_title, 
             save_path=f"figs/time/{figure_file}")
